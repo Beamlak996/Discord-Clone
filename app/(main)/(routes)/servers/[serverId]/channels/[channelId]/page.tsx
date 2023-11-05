7823883485ac73410e798fe30d136c4c01bee346
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-11-05 00:52:18 -0900

    Chat Header Complete

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,45 @@
+import ChatHeader from "@/components/chat/chat-header"
+import { currentProfile } from "@/lib/current-profile"
+import { prisma } from "@/lib/db"
+import { redirectToSignIn } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
 
-const ChannelIdPage = () => {
+type ChannelIdPageProps = {
+  params: {
+    channelId: string,
+    serverId: string
+  }
+}
+
+const ChannelIdPage = async ({params}: ChannelIdPageProps) => {
+  const profile = await currentProfile()
+
+  if(!profile) return redirectToSignIn()
+
+  const channel = await prisma.channel.findUnique({
+    where: {
+      id: params.channelId
+    }
+  })
+
+  const member = await prisma.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id
+    }
+  })
+
+  if(!channel || !member) return redirect("/")
+
+
   return (
-    <div>ChannelPage</div>
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full" >
+      <ChatHeader 
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
   )
 }
 
