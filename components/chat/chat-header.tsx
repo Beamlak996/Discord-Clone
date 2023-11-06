4ef3dd760350b28ec6a620c3ce5fdf44b5e58644
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-11-06 02:12:40 -0900

    Socket IO Complete

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from "lucide-react"
 import MobileToggle from "../mobile-toggle"
 import UserAvatar from "../user-avatar"
+import SocketIndicator from "../socket-indicator"
 
 type ChatHeaderProps = {
     serverId: string
@@ -31,6 +32,9 @@ const ChatHeader = ({
       <p className="font-semibold text-md text-black dark:text-white" >
         {name}
       </p>
+      <div className="ml-auto flex items-center" >
+        <SocketIndicator />
+      </div>
     </div>
   )
 }
