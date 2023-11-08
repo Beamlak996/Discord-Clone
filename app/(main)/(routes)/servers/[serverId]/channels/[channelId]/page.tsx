d8a1a22b3e949442a7bc124db54da5017cbc95c0
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-11-08 04:06:24 -0900

    Chat Messages Component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import ChatHeader from "@/components/chat/chat-header"
 import ChatInput from "@/components/chat/chat-input"
+import ChatMessages from "@/components/chat/chat-messages"
 import { currentProfile } from "@/lib/current-profile"
 import { prisma } from "@/lib/db"
 import { redirectToSignIn } from "@clerk/nextjs"
@@ -34,16 +35,27 @@ const ChannelIdPage = async ({params}: ChannelIdPageProps) => {
 
 
   return (
-    <div className="bg-white dark:bg-[#313338] flex flex-col h-full" >
-      <ChatHeader 
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
       />
-      <div className="flex-1" > 
-        Future Messages
-      </div>
-      <ChatInput 
+      <ChatMessages
+        member={member}
+        name={channel.name}
+        chatId={channel.id}
+        type="channel"
+        apiUrl="/api/messages"
+        socketUrl="/api/socket/messages"
+        socketQuery={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+        paramKey="channelId"
+        paramValue={channel.id}
+      />
+      <ChatInput
         name={channel.name}
         type="channel"
         apiUrl="/api/socket/messages"
@@ -53,7 +65,7 @@ const ChannelIdPage = async ({params}: ChannelIdPageProps) => {
         }}
       />
     </div>
-  )
+  );
 }
 
 export default ChannelIdPage
